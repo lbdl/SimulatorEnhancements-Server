bc9ef5286d18ad8fbf89fa72cb6c67af48c7bc82
Tim Storey <tim.storey@silverrailtech.com>
2016-11-25 22:44:37 +0000

    Add file reading.

--- model.js
+++ model.js
@@ -1,12 +1,15 @@
 function Model() {
-  this.accelerometer = {
-    x : ko.observable(0.0),
-    y : ko.observable(0.0),
-    z : ko.observable(0.0)
-  };
+    this.accelerometer = {
+        x: ko.observable(0.0),
+        y: ko.observable(0.0),
+        z: ko.observable(0.0)
+    };
 
-  this.location = {
-    latitude : ko.observable(51.521732),
-    longitude : ko.observable(-0.134491)
-  };
+    this.location = {
+        latitude: ko.observable(51.521732),
+        longitude: ko.observable(-0.134491)
+    };
+
+
+
 }
--- viewModel.js
+++ viewModel.js
@@ -1,63 +1,89 @@
-
 function ViewModel() {
-  var that = this, timerHandle;
 
-  // ------ properties
-  this.model = new Model();
-  this.isPlaying = ko.observable(false);
-  this.locations = ko.observableArray();
-  this.currentLocation = ko.observable();
-  this.dataFile = ko.observable("testdata");
+    // var $ = jQuery = require('jquery');
+    // require('./jquery.csv.js');
 
-  // expose the model as a JSON property
-  this.serializedModel = ko.computed(function() {
-    return ko.toJSON(this.model);
-  }, this);
+    var that = this, timerHandle;
 
-  // ------  functions
-  this.stop = function() {
-    clearInterval(timerHandle);
-    that.isPlaying(false);
-  };
+    // ------ properties
+    this.model = new Model();
+    this.isPlaying = ko.observable(false);
+    this.locations = ko.observableArray();
+    this.currentLocation = ko.observable();
+    this.dataFile = ko.observable();
+    this.testData = ko.observable();
+    this.coordinated = ko.observableArray();
 
-  this.removeAll = function() {
-    that.locations.removeAll();
-  };
+    // expose the model as a JSON property
+    this.serializedModel = ko.computed(function () {
+        return ko.toJSON(this.model);
+    }, this);
 
-  this.play = function() {
-    var time = 0;
-    this.isPlaying(true);
-    timerHandle = setInterval(function() {
-      var locationCount = that.locations().length - 1;
-      var index = Math.floor(locationCount * time);
-      var leftLocation = that.locations()[index];
-      var rightLocation = that.locations()[index + 1];
-      var fraction = (time * locationCount - index);
-
-      that.currentLocation(google.maps.geometry.spherical.interpolate(leftLocation, rightLocation, fraction));
-
-      time += 0.01;
-      if (time>=1.0) {
+    // ------  functions
+    this.stop = function () {
         clearInterval(timerHandle);
         that.isPlaying(false);
-      }
-    }, 100);
-  };
+    };
 
-  // update the model when the currentLocation property changes
-  this.currentLocation.subscribe(function(currentLocation) {
-    this.model.location.latitude(currentLocation.lat());
-    this.model.location.longitude(currentLocation.lng());
-  }, this);
+    this.dataSet = function() {
+        if (this.dataFile) {
+            return true;
+        }else {
+            return false;
+        }
+    };
 
-  // when the model changes, send thew new data, throttled at 200ms
-  this.serializedModel.throttle(200, true).subscribe(function(newValue) {
-    console.log(newValue);
-    var request = new XMLHttpRequest();
-    request.open('POST', '/update', true);
-    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8"); 
-    request.send(newValue);
-  })
+    this.removeAll = function () {
+        that.locations.removeAll();
+    };
+
+    this.play = function () {
+        var time = 0;
+        this.isPlaying(true);
+        timerHandle = setInterval(function () {
+            var locationCount = that.locations().length - 1;
+            var index = Math.floor(locationCount * time);
+            var leftLocation = that.locations()[index];
+            var rightLocation = that.locations()[index + 1];
+            var fraction = (time * locationCount - index);
+            that.currentLocation(google.maps.geometry.spherical.interpolate(leftLocation, rightLocation, fraction));
+            time += 0.01;
+            if (time >= 1.0) {
+                clearInterval(timerHandle);
+                that.isPlaying(false);
+            }
+        }, 100);
+    };
+
+    this.loadFileData = function () {
+        if (document.querySelector('input').files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                var contents = e.target.result;
+                testData = contents.split(';');
+            };
+
+            reader.readAsText(document.querySelector('input').files[0]);
+        } else {
+            alert("Failed to load file");
+        }
+    };
+
+    // update the model when the currentLocation property changes
+    this.currentLocation.subscribe(function (currentLocation) {
+        this.model.location.latitude(currentLocation.lat());
+        this.model.location.longitude(currentLocation.lng());
+    }, this);
+
+    // when the model changes, send thew new data, throttled at 200ms
+    this.serializedModel.throttle(200, true).subscribe(function (newValue) {
+        console.log(newValue);
+        var request = new XMLHttpRequest();
+        request.open('POST', '/update', true);
+        request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        request.send(newValue);
+    })
 }
 
 var viewModel = new ViewModel();
